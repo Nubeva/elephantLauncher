633c695d0a3d9bfbc75e8c834d614fe3c4ec7ad3
Greig Bannister <greig@nubeva.com>
2018-11-16 20:17:50 +1100

    operational

--- runit.sh
+++ runit.sh
@@ -10,9 +10,11 @@ do
 	fi
 done
 
+baseurl="https://i.nuos.io/${scriptdir}api/1.1/wf/"
+
 sudo apt-get install -y docker.io
 
-sudo docker ps | grep -q elephant || sudo docker run -p80:80 -v /:/host -v /var/run/docker.sock:/var/run/docker.sock --privileged --name elephant -d --restart=on-failure --net=host nubeva/nuagent:eleph --accept-eula --nutoken $nutoken  --vxlan-port 4789 --vnet $vnetid --baseurl https://i.nuos.io/api/1.1/wf/  --ruok-port 80
+sudo docker ps | grep -q elephant || sudo docker run -p80:80 -v /:/host -v /var/run/docker.sock:/var/run/docker.sock --privileged --name elephant -d --restart=on-failure --net=host nubeva/nuagent:eleph --accept-eula --nutoken $nutoken  --vxlan-port 4789 --vnet $vnetid --baseurl $baseurl  --ruok-port 80
 
-sudo docker inspect vagent > /dev/null 2>&1 || sudo docker run --name vagent -d -e AZURE_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID -e AZURE_TENANT_ID=$AZURE_TENANT_ID -e AZURE_CLIENT_ID=$AZURE_CLIENT_ID -e AZURE_CLIENT_SECRET=$AZURE_CLIENT_SECRET nubeva/vagent:eleph --vnet $vnetid  --target $loadBalancerFrontEndTarget --nutoken $nutoken --baseurl https://i.nuos.io/api/1.1/wf/ --resource-group $resourceGroup --location $location
+sudo docker inspect vagent > /dev/null 2>&1 || sudo docker run --name vagent -d -e AZURE_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID -e AZURE_TENANT_ID=$AZURE_TENANT_ID -e AZURE_CLIENT_ID=$AZURE_CLIENT_ID -e AZURE_CLIENT_SECRET=$AZURE_CLIENT_SECRET nubeva/vagent:eleph --vnet $vnetid  --target $loadBalancerFrontEndTarget --nutoken $nutoken --baseurl $baseurl --resource-group $resourceGroup --location $location
 
